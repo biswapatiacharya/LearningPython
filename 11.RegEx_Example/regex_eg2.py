cbb65d5b6f45f8b39367b8a08e262605942e381d
bacharya <biswapati.lipun@gmail.com>
2016-08-29 16:38:21 -0400

    regex 1 updated

--- 11.RegEx_Example/regex_eg2.py
+++ 11.RegEx_Example/regex_eg2.py
@@ -7,18 +7,19 @@ import re
 
 s=input("Please write the string with duplicate charcters:")
 print(type(s))
-if s == str:
-    a = re.compile(r'(([a-z])\2{1,})',re.I)
-    r= a.sub(r'\2',s)
+if type(s) == str:
+    # a = re.compile(r'(([a-z])\2{1,})',re.I)
+    a = re.compile(r'((.*)\2{1,})',re.I)
+    r = a.sub(r'\2',s)
     print(r)
 
     # r= re.sub(r'(([a-z])\2{1,})',r'\2',s)
 
-    ''''
-    a = re.compile(r'([a-z])\1+',re.I)
-    r = a.sub(r'\1', s)
-    print(r)
-    '''
+    #
+    # a = re.compile(r'([a-z])\1+',re.I)
+    # r = a.sub(r'\1', s)
+    # print(r)
+
 else:
     print("Invalid string:")
 
