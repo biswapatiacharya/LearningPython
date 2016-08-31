a6c6264f367de633a68e1b57946b07682db9067c
biswapatiacharya <biswapati.lipun@gmail.com>
2016-08-31 00:22:20 -0400

    json nested dict

--- 12.Json_Example/json_test.py
+++ 12.Json_Example/json_test.py
@@ -1,10 +1,20 @@
 import json
+
+
 f = open("C://python_test//json_test.json","r")
 b=f.read()
 #print (b)
 c={}
 c= json.loads(b)
+print(c.keys())
+#  print(c['Satellite_Constellation'].keys())
 
-print(c['data'][0].keys())
+# print(c['Satellite_Constellation'][0].keys()
+
+# print(c['Satellite_Constellation']['Satellites'][0]['AllSatellites'][0]['SatLong'])
+
+print(type(c['Satellite_Constellation']['Satellites'][0]['AllSatellites']))
+# print(type(c['Satellite_Constellation']))
+
 
 #print(c['data'][0]['obj_id'])
